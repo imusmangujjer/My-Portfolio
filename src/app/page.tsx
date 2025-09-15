7b232b79ccea7e59cfa9b59d17e639d2efcbf7cb
Usman Gujjer <usmangujjer945@gmail.com>
2025-09-15 23:46:49 +0000

    Make entire page fit to width and centered.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
       <main className="flex-1">
+        <Hero />
         <div className="container">
-          <Hero />
           <About />
           <Skills />
           <Projects />
