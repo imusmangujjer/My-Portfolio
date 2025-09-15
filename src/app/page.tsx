2de76655fea5bcb024bb559815d8e447fd550f8c
Usman Gujjer <usmangujjer945@gmail.com>
2025-09-15 23:45:50 +0000

    This is what i'm getting as you can see it's not centerally aligned and

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,16 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1 container">
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Experience />
-        <Education />
-        <Contact />
+      <main className="flex-1">
+        <div className="container">
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Experience />
+          <Education />
+          <Contact />
+        </div>
       </main>
       <Footer />
     </div>
