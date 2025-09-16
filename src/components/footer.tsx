3dfe0a505f7edebecba095f6aba9abea5a019aa3
Usman Gujjer <usmangujjer945@gmail.com>
2025-09-16 00:43:09 +0000

    Make the similar changes in footer to aligned it like header.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,9 +6,9 @@ export function Footer() {
 
   return (
     <footer className="border-t">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+      <div className="flex w-full h-14 items-center justify-between px-4 md:px-8">
+        <div className="flex items-center">
+          <p className="text-sm text-muted-foreground">
             &copy; {currentYear} {USER_DETAILS.name}. All Rights Reserved.
           </p>
         </div>
